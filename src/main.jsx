c5c6d48892bc55b15e097c5d4ec0e08be4b3eb9d
seokam36 <seokam36@gmail.com>
2026-09-15 09:38:25 +0900

    react

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Component2 from "./example/day02/Component2.jsx";
 import Component3 from "./example/day02/Component3.jsx";
 import Component5 from "./example/day01/Component5.jsx";
 import Practice1 from "./example/practice1/Practice1.jsx";
+import ProductManager from "./example/totalpractice1/ProductManager.jsx";
 // [필수] 2. index.html 에서 root 마크업 가져오기
 const root = document.querySelector('#root')
 // [필수] 3. 거져온 root 마크업을 createRoot 함수에 전달
@@ -19,4 +20,4 @@ const create = createRoot(root)
 //create.render(<MyMarkUp></MyMarkUp>)
 //create.render(<Component1></Component1>)
 //create.render(<Component2></Component2>)
-create.render(<Component3></Component3>)
+create.render(<ProductManager></ProductManager>)
